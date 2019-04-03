4ab684d8c24e26c8b4d0d5ab0b05abf4ff9b6a3e
Ivan <ivansatya10@gmail.com>
2019-04-03 17:20:01 +0700

    section 3 chapter 29

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
+// Section 3, 29
+
 package main
 
 import "github.com/gin-gonic/gin"
-import "fmt"
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		fmt.Println("Hi Ivan S")
 
+	r.GET("/", func(c *gin.Context) {
+		cards := newDeck()
+		hand, remainingCards := deal(cards, 5)
+
+		// cards.saveToFile("Cards")
+		cards2 := newDeckFromFile("Cards")
+
+		c.JSON(200, gin.H{
+			"message": "pong",
+			"data": gin.H{
+				"cards":          cards,
+				"hand":           hand,
+				"remainingCards": remainingCards,
+				"string":         cards.toString(),
+				"cards2":         cards2,
+			},
+		})
+	})
+
+	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
